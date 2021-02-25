ea2d60783bfd4a2cf13702c3130bba4b60f630ea
Brunao <hello@brunao.me>
2021-02-25 18:55:50 -0300

    Adding favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3393ffd9f76eefcf065f";
+/******/ 	var hotCurrentHash = "e5f3a67927d32f733d1f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

2df16368eba9ad3ab74ff525fa47b2a0fecc5881
Brunao <hello@brunao.me>
2021-03-17 12:23:41 -0300

    shafraaz site

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "43913cff9a3d1dbf69d1";
+/******/ 	var hotCurrentHash = "74e3af4480bba58c4a42";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
